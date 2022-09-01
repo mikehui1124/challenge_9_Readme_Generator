cb87f64885281a17c6cf113a575b2d044c7b77e2
mikehui1124 <105307687+mikehui1124@users.noreply.github.com>
2022-09-01 16:58:19 +0100

    Update generateMarkdown.js
    To include GitHub URL

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,3 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  var badge='';
-  var licenseChoice='';
-  var licenseText='';
-
-  if (license !== 'No License') {
-    badge = `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
-    licenseChoice = `This project is licensed under the ${license} license`
-    licenseText ="Copyright (c) 2022 [Your Full Name]\n"+ 
-    "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the Software), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n" + "\n" +
-    "The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software."
-
-  } else if (license == 'No License') {
-    licenseChoice = 'No License';
-  }
-}
-
-
 // Create a function to generate markdown for README
 // create URL for badge icon according to the selected license
 // add 'No License' when no license is selected
@@ -76,6 +56,7 @@ function generateMarkdown({gitHub, email, projTitle, description,install,test,us
 
     ## Questions
       GitHub User Profile: ${gitHub}
+      GitHub URL: https://github.com/${gitHub} 
       Email address: ${email}
     
     `;
